37463dd1d3213b29c58ca979bd047c94124ab3a0
Lars <lars.bischhausen@gmail.com>
2024-04-17 15:52:16 +0200

    WIP header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { LanguageProviderService } from './common/services/languageProvider.service';
-import { LanguageSwitcherComponent } from './common/components/language-switcher/language-switcher.component';
 import { NavigationComponent } from './common/components/navigation/navigation.component';
+import { ParallaxWrapperComponent } from './common/components/parallax-wrapper/parallax-wrapper.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TranslateModule, LanguageSwitcherComponent, NavigationComponent],
+  imports: [RouterOutlet, TranslateModule, ParallaxWrapperComponent, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/common/components/parallax-wrapper/parallax-wrapper.component.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parallax-wrapper',
+  templateUrl: './parallax-wrapper.component.html',
+  styleUrls: ['./parallax-wrapper.component.scss'],
+  standalone: true,
+  imports: []
+})
+export class ParallaxWrapperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
