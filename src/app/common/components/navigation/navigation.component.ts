ad66bcec7b5c2f2cc6019b2367f08ef4e47cd83d
Lars <lars.bischhausen@gmail.com>
2024-04-17 11:22:27 +0200

    feat: complete navigation

--- src/app/common/components/navigation/navigation.component.ts
+++ src/app/common/components/navigation/navigation.component.ts
@@ -1,26 +1,16 @@
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import { Component, OnInit } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { LanguageSwitcherComponent } from '../language-switcher/language-switcher.component';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
   standalone: true,
-  imports: []
+  imports: [TranslateModule, LanguageSwitcherComponent]
 })
 export class NavigationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-    document.body.style.setProperty('--navbar-width', "0");
+  ngOnInit(): void {
   }
-
-  open() : void{
-    document.body.style.setProperty('--navbar-width', "250px");
-  }
-
-  close() : void{
-    document.body.style.setProperty('--navbar-width', "0");
-  }
-
 }
